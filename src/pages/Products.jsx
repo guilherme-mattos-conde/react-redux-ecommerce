72d7208946d8cc20aa8a34a794c4b94bb0cec073
Guilherme Mattos Conde <guilherme.mattos.conde@gmail.com>
2026-03-11 14:22:33 -0300

    criação da pagina de produtos

---
+++ src/pages/Products.jsx
@@ -0,0 +1,12 @@
+import ProductsList from "../components/products/ProductsList"
+import CustomerData from "../components/forms/CustomerData";
+
+const Products = () => {
+
+    return <div className="products-container">
+        <ProductsList />
+        <CustomerData />
+    </div>
+}
+
+export default Products;
