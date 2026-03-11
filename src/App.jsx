b63ebc219e974c488d16dbcf5eb906afd808f101
Guilherme Mattos Conde <guilherme.mattos.conde@gmail.com>
2026-03-11 13:55:46 -0300

    produtos carregados no inicio da aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,28 @@
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+
+import { showAlert } from "./store/alert/alertSlice";
+import { loadProducts } from "./store/product/productSlice";
+
 function App() {
-    
-  return <>
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const products = await getProducts();
+                dispatch(loadProducts(products));
+            } catch (error) {
+                dispatch(showAlert({
+                    message: "Erro ao carregar produtos!",
+                    severity: "error"
+                }));
+            }
+        }
+
+        fetchProducts();
+    }, [dispatch]);
+    return <>
     </>
 }
 
