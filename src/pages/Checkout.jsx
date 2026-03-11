01db17c2a5db920c27ef695cbffb6671223a5645
Guilherme Mattos Conde <guilherme.mattos.conde@gmail.com>
2026-03-11 14:27:51 -0300

    criação da pagina de checkout

---
+++ src/pages/Checkout.jsx
@@ -0,0 +1,20 @@
+import { useSearchParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import { orderSelectors } from "../store/order/orderSlice";
+
+import CardCheckout from "../components/checkout/CardCheckout";
+import OrderNotFound from "../components/checkout/OrderNotFound";
+
+const Checkout = () => {
+    const [searchParams] = useSearchParams();
+    const id = Number(searchParams.get("id"));
+
+    const order = useSelector(state => orderSelectors.selectById(state, id));
+
+    return <div className="checkout-container">
+        {order ? <CardCheckout order={order} /> : <OrderNotFound />}
+    </div>
+};
+
+export default Checkout;
